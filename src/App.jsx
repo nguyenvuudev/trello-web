40c6f60b26fb800677b6f37edd2b7631ac062d6a
hoangvu17tb <nguyenvuu17tb@gmail.com>
2023-12-01 20:29:21 +0700

    Update App Typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Button from '@mui/material/Button'
+import Typography  from '@mui/material/Typography'
 
 function App() {
   return (
     <>
       <div>nguyenvudev</div>
+
+      <Typography variant="body2" color="text.secondary">Hello World!</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
